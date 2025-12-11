55c15dc83eda7491aa4a8ee2e07b28efe0d2bf2c
manar saighi <manarsaighi2003@icloud.com>
2025-12-11 23:16:46 +0000

    linking bee w flower

--- bee.gd
+++ bee.gd
@@ -1,20 +1,45 @@
 extends Node3D
 
-var center := Vector3.ZERO
+var center: Vector3
 var radius := 2.0
 var speed := 1.5
 var angle := 0.0
 
+var flying_to_target := false
+var target_position: Vector3 = Vector3.ZERO
+
 func _ready():
-	center = global_transform.origin  # Bee orbits where it starts
+	center = global_transform.origin
 
 func _process(delta):
+	if flying_to_target:
+		_fly_to_flower(delta)
+	else:
+		_circle(delta)
+
+func _circle(delta):
 	angle += speed * delta
 
-	var x = center.x + radius * cos(angle)
-	var z = center.z + radius * sin(angle)
-	var y = center.y  # constant height
+	var orbit_pos = Vector3(
+		center.x + radius * cos(angle),
+		center.y,
+		center.z + radius * sin(angle)
+	)
 
-	global_transform.origin = Vector3(x, y, z)
-
+	global_transform.origin = orbit_pos
 	look_at(center, Vector3.UP)
+
+
+func _fly_to_flower(delta):
+	var dir := (target_position - global_transform.origin)
+	var dist := dir.length()
+
+	if dist < 0.2:
+		# Arrived at the flower
+		center = target_position
+		flying_to_target = false
+		return
+
+	dir = dir.normalized()
+	global_transform.origin += dir * speed * delta
+	look_at(target_position, Vector3.UP)
--- flower.gd
+++ flower.gd
@@ -3,3 +3,20 @@ extends Node3D
 func _on_area_3d_input_event(_camera, event, _position, _normal, _shape_idx):
 	if event is InputEventMouseButton and event.pressed:
 		print("Flower clicked:", name)
+
+		var bees = get_tree().get_nodes_in_group("Bee")
+		if bees.size() == 0:
+			return
+
+		var closest_bee = null
+		var closest_dist = INF
+		var click_pos = global_transform.origin
+
+		for bee in bees:
+			var dist = bee.global_transform.origin.distance_to(click_pos)
+			if dist < closest_dist:
+				closest_dist = dist
+				closest_bee = bee
+
+		if closest_bee:
+			closest_bee.target_position = click_pos
