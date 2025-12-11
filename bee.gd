a4b9b21e75449c8b5c5867bc494893ccdf55796c
manar saighi <manarsaighi2003@icloud.com>
2025-12-11 20:06:05 +0000

    flower model

--- bee.gd
+++ bee.gd
@@ -1,23 +1,20 @@
 extends Node3D
 
-var speed = 3.0
-var wander_radius = 5.0
-var target_position: Vector3
+var center := Vector3.ZERO
+var radius := 2.0
+var speed := 1.5
+var angle := 0.0
 
 func _ready():
-	pick_new_target()
+	center = global_transform.origin  # Bee orbits where it starts
 
 func _process(delta):
-	var direction = (target_position - global_transform.origin).normalized()
-	global_translate(direction * speed * delta)
-	look_at(target_position, Vector3.UP)
-	if global_transform.origin.distance_to(target_position) < 0.5:
-		pick_new_target()
+	angle += speed * delta
 
-func pick_new_target():
-	var random_offset = Vector3(
-		randf_range(-wander_radius, wander_radius),
-		randf_range(0.5, wander_radius),
-		randf_range(-wander_radius, wander_radius)
-	)
-	target_position = global_transform.origin + random_offset
+	var x = center.x + radius * cos(angle)
+	var z = center.z + radius * sin(angle)
+	var y = center.y  # constant height
+
+	global_transform.origin = Vector3(x, y, z)
+
+	look_at(center, Vector3.UP)
