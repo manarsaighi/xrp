9cd5abd552345a08549631aaff4520877695dd2e
manar saighi <manarsaighi2003@icloud.com>
2025-12-11 23:04:39 +0000

    error fixes, added another bug

--- flower.gd
+++ flower.gd
@@ -1,5 +1,5 @@
 extends Node3D
 
-func _on_area_3d_input_event(camera, event, click_pos, normal, shape_idx):
+func _on_area_3d_input_event(_camera, event, _position, _normal, _shape_idx):
 	if event is InputEventMouseButton and event.pressed:
 		print("Flower clicked:", name)
