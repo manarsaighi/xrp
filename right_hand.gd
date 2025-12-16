09a5ca9314ba7398d69577e0294faa98a8192ba0
manar saighi <manarsaighi2003@icloud.com>
2025-12-16 20:22:40 +0000

    adding wrist display

--- right_hand.gd
+++ right_hand.gd
@@ -1,13 +1,13 @@
 extends XRController3D
 
-@export var pointer: NodePath  # Optional: your XRToolsFunctionPointer
+@export var pointer: NodePath 
 var pointer_node
 
 func _ready():
-	# If you want to connect to the button_pressed signal:
+	
 	button_pressed.connect(_on_button_pressed)
 
-	# Optional: cache the pointer node if assigned
+	
 	if pointer != null:
 		pointer_node = get_node(pointer)
 
