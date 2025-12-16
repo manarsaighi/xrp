82960e724c73f0b2b15c51a376ad7f24adb4f351
manar saighi <manarsaighi2003@icloud.com>
2025-12-16 23:25:26 +0000

    grass texture and bee placements

--- main.gd
+++ main.gd
@@ -1,6 +1,5 @@
 extends Node3D
 
-
 var xr_interface: XRInterface
 
 func _ready():
@@ -9,8 +8,13 @@ func _ready():
 	if xr_interface and xr_interface.is_initialized():
 		print("success")
 		
+		# Set VR render scale per eye (0.5–1.0)
+		xr_interface.render_scale = 0.7  # adjust as needed
+		
+		# Disable VSync for smoother VR streaming
 		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
 		
+		# Enable XR for this viewport
 		get_viewport().use_xr = true
 	else:
 		print("not working")
